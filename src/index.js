f58fe9f06961c4853a2222dfb1ee2257c75421cf
Luiz <ltonza@gmail.com>
2018-12-04 00:42:46 -0200

    Atualizado até aula: Estilos com SASS no ReactJS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-const soma = (a, b) => a + b;
+import React, { Component, Fragment } from "react";
 
-alert(soma(5, 2));
+import { render } from "react-dom";
+
+import Button from "./Button";
+
+import "./style.scss";
+
+class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h1 className="title">Hello Rocketseat</h1>
+        <h2>{this.state.counter}</h2>
+        <Button onClick={this.handleClick}>Enviar</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
